c8ea83c76e48af3899465a3df4e5e67877a3e7ac
hamzahassnaain <hamzahassanain067@gmail.com>
2025-08-28 12:35:49 +0300

    np

--- includes/healpers.hpp
+++ includes/healpers.hpp
@@ -12,17 +12,17 @@
 
 namespace hh_json::maker
 {
-    std::shared_ptr<hh_json::JsonObject> make_string(const std::string &value)
+    inline std::shared_ptr<hh_json::JsonObject> make_string(const std::string &value)
     {
         return std::make_shared<hh_json::JsonString>(value);
     }
 
-    std::shared_ptr<hh_json::JsonObject> make_number(double value)
+    inline std::shared_ptr<hh_json::JsonObject> make_number(double value)
     {
         return std::make_shared<hh_json::JsonNumber>(value);
     }
 
-    std::shared_ptr<hh_json::JsonObject> make_boolean(bool value)
+    inline std::shared_ptr<hh_json::JsonObject> make_boolean(bool value)
     {
         return std::make_shared<hh_json::JsonBoolean>(value);
     }
@@ -31,7 +31,7 @@ namespace hh_json::maker
 
 namespace hh_json::getter
 {
-    bool get_boolean(const std::shared_ptr<hh_json::JsonObject> &obj)
+    inline bool get_boolean(const std::shared_ptr<hh_json::JsonObject> &obj)
     {
         if (auto boolean = std::dynamic_pointer_cast<hh_json::JsonBoolean>(obj))
         {
@@ -40,7 +40,7 @@ namespace hh_json::getter
         throw std::runtime_error("Not a boolean");
     }
 
-    double get_number(const std::shared_ptr<hh_json::JsonObject> &obj)
+    inline double get_number(const std::shared_ptr<hh_json::JsonObject> &obj)
     {
         if (auto number = std::dynamic_pointer_cast<hh_json::JsonNumber>(obj))
         {
@@ -49,7 +49,7 @@ namespace hh_json::getter
         throw std::runtime_error("Not a number");
     }
 
-    std::string get_string(const std::shared_ptr<hh_json::JsonObject> &obj)
+    inline std::string get_string(const std::shared_ptr<hh_json::JsonObject> &obj)
     {
         if (auto str = std::dynamic_pointer_cast<hh_json::JsonString>(obj))
         {
@@ -58,7 +58,7 @@ namespace hh_json::getter
         throw std::runtime_error("Not a string");
     }
 
-    std::vector<std::shared_ptr<hh_json::JsonObject>> get_array(const std::shared_ptr<hh_json::JsonObject> &obj)
+    inline std::vector<std::shared_ptr<hh_json::JsonObject>> get_array(const std::shared_ptr<hh_json::JsonObject> &obj)
     {
         if (auto array = std::dynamic_pointer_cast<hh_json::JsonArray>(obj))
         {
